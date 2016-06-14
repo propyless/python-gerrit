ae44568de3e92a7144a201551f0584e20f58cefd
Takeshi Larsson <takeshi.p.larsson@gmail.com>
2016-06-14 21:31:38 +0200

    split functional tests and unit tests

--- tests/functional_tests/functional_tests_gerrit.py
+++ tests/functional_tests/functional_tests_gerrit.py
@@ -1,13 +1,14 @@
 #!/bin/env python
 
 import unittest
+
 import os
 import yaml
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 
 from gerrit import Gerrit
 
